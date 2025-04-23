9a6d531d270d521901848a949135c39afffe0a54
BaitemirAsanbaev <baitemir1706@gmail.com>
2025-04-24 00:00:13 +0600

    added employee interface and position and department enums and mapped them logically

--- src/services/EmployeesService.ts
+++ src/services/EmployeesService.ts
@@ -1,11 +1,12 @@
+import { Employee } from "@/types/Emplyee";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const EmployeesService = createApi({
   reducerPath: "employees",
   baseQuery: fetchBaseQuery({ baseUrl: "https://localhost:3000/employees" }),
   endpoints: (builder) => ({
-    getEmployees: builder.query({query: () => "/" }),
-    createEmployees: builder.mutation({
+    getEmployees: builder.query<Employee[], void>({query: () => "/" }),
+    createEmployees: builder.mutation<Employee, Employee>({
       query: (newEmployee) => ({
         url: "/",
         method: "POST",
---
+++ src/types/Emplyee.ts
@@ -0,0 +1,47 @@
+export interface Employee{
+    "id": number,
+    "name": string,
+    "email": string,
+    "age": number,
+    "position": Position,
+    "department": Department
+}
+
+export enum Position {
+    ManualQA = "Manual QA",
+    AutomationQA = "Automation QA",
+    FrontendDeveloper = "Frontend Developer",
+    BackendDeveloper = "Backend Developer",
+    DevOps = "DevOps",
+    ProjectManager = "Project Manager",
+    HR = "HR",
+    TeamLead = "Team Lead",
+  }
+  
+  export enum Department {
+    QA = "QA",
+    Development = "Development",
+    Management = "Management",
+  }
+  
+
+export const PositionDepartmentMap: Record<Position, Department> = {
+    [Position.ManualQA]: Department.QA,
+    [Position.AutomationQA]: Department.QA,
+    [Position.FrontendDeveloper]: Department.Development,
+    [Position.BackendDeveloper]: Department.Development,
+    [Position.DevOps]: Department.Development,
+    [Position.ProjectManager]: Department.Management,
+    [Position.HR]: Department.Management,
+    [Position.TeamLead]: Department.Management,
+  };
+  
+
+// const baitemir: Employee = {
+//     id:1,
+//     name:"Baitemir",
+//     email:"bait@gmail.com",
+//     age:20,
+//     position: Position.DevOps,
+//     department: PositionDepartmentMap[Position.DevOps],    
+// }
