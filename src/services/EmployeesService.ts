cecc95d71a3300058ca7593829f4bae7d924991f
BaitemirAsanbaev <baitemir1706@gmail.com>
2025-04-24 21:16:45 +0600

    api into const

--- src/services/EmployeesService.ts
+++ src/services/EmployeesService.ts
@@ -1,9 +1,10 @@
+import { api } from "@/const/api";
 import { Employee } from "@/types/Emplyee";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const EmployeesService = createApi({
   reducerPath: "employees",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/employees" }),
+  baseQuery: fetchBaseQuery({ baseUrl: `${api}/employees` }),
   endpoints: (builder) => ({
     getEmployees: builder.query<Employee[], void>({ query: () => "/" }),
     getEmployeeById: builder.query<Employee, string>({ query: (id) => `/${id}` }),
